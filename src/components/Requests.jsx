faedcb952bb791f4363420693506e795fb90180e
Sachin Kuldeep <sachinkuldeep201@gmail.com>
2025-02-27 23:57:23 +0530

    accept/reject connection requests

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { BASE_URL } from "../utils/constants";
-import { addRequests } from "../utils/requestSlice";
+import { addRequests, removeRequests } from "../utils/requestSlice";
 
 const Requests = () => {
   const requests = useSelector((store) => store.requests);
@@ -17,13 +17,25 @@ const Requests = () => {
     } catch (error) {}
   };
 
+  const reviewRequest = async (status, _id) => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/request/review" + "/" + status + "/" + _id,
+        {},
+        { withCredentials: true }
+      );
+      dispatch(removeRequests(_id));
+    } catch (error) {}
+  };
+
   useEffect(() => {
     fetchRequests();
   }, []);
 
   if (!requests) return;
 
-  if (requests.length === 0) return <h1>No Requests found.</h1>;
+  if (requests.length === 0)
+    return <h1 className="flex justify-center">No Requests found.</h1>;
 
   return (
     <div className="flex flex-col items-center my-20">
@@ -37,26 +49,36 @@ const Requests = () => {
           <div
             key={_id}
             className="flex m-4 p-4 justify-between items-center rounded-lg bg-base-300 w-1/2"
-          > 
+          >
             <div className="flex">
-            <div>
-              <img
-                className="w-20 h-20 rounded-full"
-                src={photoUrl}
-                alt="photo"
-              />
-            </div>
-            <div className="text-left mx-4">
-              <h2 className="font-bold text-xl">
-                {firstName + " " + lastName}
-              </h2>
-              {age && gender && <p>{age + ", " + gender}</p>}
-              <p>{about}</p>
-            </div>
+              <div>
+                <img
+                  className="w-20 h-20 rounded-full"
+                  src={photoUrl}
+                  alt="photo"
+                />
+              </div>
+              <div className="text-left mx-4">
+                <h2 className="font-bold text-xl">
+                  {firstName + " " + lastName}
+                </h2>
+                {age && gender && <p>{age + ", " + gender}</p>}
+                <p>{about}</p>
+              </div>
             </div>
             <div className="flex">
-              <button className="btn btn-primary mx-2">Primary</button>
-              <button className="btn btn-secondary mx-2">Secondary</button>
+              <button
+                className="btn btn-primary mx-2"
+                onClick={() => reviewRequest("accepted", request._id)}
+              >
+                Accept
+              </button>
+              <button
+                className="btn btn-secondary mx-2"
+                onClick={() => reviewRequest("rejected", request._id)}
+              >
+                Reject
+              </button>
             </div>
           </div>
         );
