9789763a8b14ef6e23f5df30802f1619a5418336
Sachin Kuldeep <sachinkuldeep201@gmail.com>
2025-07-03 13:18:00 +0530

    NavBar

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import NavBar from "./Navbar";
+import NavBar from "./NavBar";
 import { Outlet, useNavigate } from "react-router-dom";
 import Footer from "./Footer";
 import { useDispatch, useSelector } from "react-redux";
