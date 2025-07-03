1aa0b4ec4e30122924c8f42fc43229d7433cf7ec
Sachin Kuldeep <sachinkuldeep201@gmail.com>
2025-07-03 13:32:19 +0530

    Base URL for Vercel

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,7 @@
+// export const BASE_URL =
+//   location.hostname === "localhost" ? "http://localhost:7777" : "/api";
+
+
+// For vercel
 export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:7777" : "/api";
+  location.hostname === "localhost" ? "http://localhost:7777" : "https://devtinder-c2rf.onrender.com";
